cf409ed3b6bedc6acd6332ebd07ea92659bc3c7a
John Letey <johnletey@gmail.com>
2021-03-31 14:06:01 +0100

    feat: use the cache in the latest price function

--- src/price.ts
+++ src/price.ts
@@ -1,66 +1,39 @@
-import Arweave from "arweave";
-import ArDB from "ardb";
 import moment, { Moment } from "moment";
+import axios from "axios";
 
 export const latestPrice = async (
   id: string,
-  client: Arweave,
-  orders?: { rate: number; timestamp: number }[],
   high?: Moment
 ): Promise<number> => {
-  if (!orders) {
-    const gql = new ArDB(client);
-    const res = await gql
-      .search()
-      .tag("Exchange", "Verto")
-      .tag("Type", "Buy")
-      .tag("Token", id)
-      .findAll();
-
-    orders = [];
-    // @ts-ignore
-    for (const edge of res) {
-      const confirmation: any = await gql
-        .search()
-        .tag("Exchange", "Verto")
-        .tag("Type", "Confirmation")
-        .tag("Match", edge.node.id)
-        .findOne();
-
-      if (confirmation.length === 1) {
-        const node = confirmation[0].node;
-        const received = node.tags.find((tag: any) => tag.name === "Received");
-
-        if (received) {
-          const amnt = Math.floor(parseFloat(received.value.split(" ")[0]));
-
-          if (amnt !== 0) {
-            orders.push({
-              rate: parseFloat(edge.node.quantity.ar) / amnt,
-              timestamp: node.block
-                ? node.block.timestamp
-                : parseInt(new Date().getTime().toString().slice(0, -3)),
-            });
-          }
-        }
-      }
-    }
-  }
-
   if (!high) {
     high = moment().add(1, "days").hours(0).minutes(0).seconds(0);
   }
   const low = high.clone().subtract(1, "days");
 
-  const filtered = orders
-    .filter(
-      (order) =>
-        order.timestamp <= high!.unix() && order.timestamp >= low.unix()
-    )
-    .map((order) => order.rate);
-  if (filtered.length === 0) {
-    return await latestPrice(id, client, orders, low);
+  const { data: orders } = await axios.get(
+    `https://v2.cache.verto.exchange/orders?token=${id}&from=${low.unix()}&to=${high.unix()}`
+  );
+
+  if (orders.length === 0) {
+    return await latestPrice(id, low);
   } else {
-    return filtered.reduce((a, b) => a + b, 0) / filtered.length;
+    const parsed = [];
+    for (const order of orders) {
+      if (order.status === "success") {
+        const input = order.input.split(" ")[0];
+        const inputUnit = order.input.split(" ")[1];
+        const output = order.output.split(" ")[0];
+
+        if (inputUnit === "AR") {
+          parsed.push(input / output);
+        }
+      }
+    }
+
+    if (parsed.length) {
+      return parsed.reduce((a, b) => a + b, 0) / parsed.length;
+    } else {
+      return await latestPrice(id, low);
+    }
   }
 };
